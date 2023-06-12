7abd826f9d8d510540ae0f70f5a5b4f7ae5dd179
Marten de Bruijn <martendebruijn@outlook.com>
2023-06-12 19:45:01 +0200

    vue: simpler call signature

--- src/vue.ts
+++ src/vue.ts
@@ -1,4 +1,4 @@
-export type PropsOf<C extends { new (...args: never[]): void }> =
+export type PropsOf<C extends abstract new (...args: never[]) => void> =
   InstanceType<C> extends { $props: unknown }
     ? InstanceType<C>['$props']
     : never
