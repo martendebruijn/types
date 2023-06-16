93c498fa01a9f6fb1c8fe5b7b06e399627ecd633
Marten de Bruijn <martendebruijn@outlook.com>
2023-06-16 19:13:51 +0200

    vue: add doc comments

--- src/vue.ts
+++ src/vue.ts
@@ -1,3 +1,14 @@
+/**
+ * Get the prop types of a given component
+ *
+ * @example
+ * // Get all prop types
+ * PropsOf<MyComponent>
+ *
+ * // Get type of specific prop
+ * PropsOf<MyComponent>["prop"]
+ *
+ */
 export type PropsOf<C extends abstract new (...args: never[]) => void> =
   InstanceType<C> extends { $props: unknown }
     ? InstanceType<C>['$props']
