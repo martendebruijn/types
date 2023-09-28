be2d74d5ca8e1a9abd9dbb797dd49449df8a0233
Marten de Bruijn <martendebruijn@outlook.com>
2023-09-28 15:57:21 +0200

    vue: rewritten tests from jest to vitest

--- src/vue.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from '@jest/globals'
-import { expectTypeTestsToPassAsync } from 'jest-tsd'
-
-describe('PropsOf', () => {
-  it('should not produce static type errors', async () => {
-    await expectTypeTestsToPassAsync(__filename)
-  })
-})
