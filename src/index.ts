cccf95b8c7bdeb9a0922ea42b025c7ade02cef5f
Marten de Bruijn <martendebruijn@outlook.com>
2023-10-06 21:29:08 +0200

    ignore prettier on long imports and exports

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
-import {
-  isDefined,
-  isString,
-  isNumber,
-  isBigInt,
-  isBool,
-  isSymbol,
-  isNull,
-} from './guards'
+import { isDefined, isString, isNumber, isBigInt, isBool, isSymbol, isNull, } from './guards' /* prettier-ignore */
 import { generateError } from './utils'
 
 import type { JsonValue, JSONValue } from './JSON'
 import type { Tuple } from './tuple'
 import type { PropsOf } from './vue'
 
-export {
-  isDefined,
-  isString,
-  isNumber,
-  isBigInt,
-  isBool,
-  isSymbol,
-  isNull,
-  generateError,
-}
+export { isDefined, isString, isNumber, isBigInt, isBool, isSymbol, isNull, generateError } /* prettier-ignore */
 export type { JsonValue, JSONValue, Tuple, PropsOf }
